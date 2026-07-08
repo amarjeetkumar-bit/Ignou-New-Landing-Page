7f4b2bb14c77bac2b36181eefc0dd56d531ed634
amarjeetkumar-bit <amarjeet.kumar@peupskill.com>
2026-07-08 15:06:33 +0530

    Add files via upload

---
+++ thankyou.php
@@ -0,0 +1,213 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/assets/images/favicon.png" sizes="32x32" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <title>Ignou Online</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/responsive.css">
+
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1093174299641215');
+fbq('track', 'PageView');
+fbq('track', 'LeadNew');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1093174299641215&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '793344013718056');
+fbq('track', 'PageView');
+fbq('track', 'LeadNew');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=793344013718056&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11059631146"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11059631146');
+</script>
+
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1397081825797750');
+fbq('track', 'PageView');
+fbq('track', 'Lead_New');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1397081825797750&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+
+</head>
+
+<body>
+
+ 
+  <section class="py-5" style="background: aliceblue;">
+    <div class="container pt-4 pb-md-5">
+      <div class="text-center">
+        <a href="#" class="d-flex align-items-center justify-content-center mb-4 bg-white px-2 mx-auto rounded shadow-sm" style="width: max-content;">
+          <img src="/assets/images/logo.jpg" width="120px" alt="Logo">
+          <!-- <div class="text-dark border-start ps-1" style="font-size: 14px;">Information By <span class="d-block">Learningshala.com</span></div> -->
+        </a>
+        <h1 class="fw-bold">THANK YOU !</h1>
+        <h5 class="">Our Senior Counsellor Will Get Back To You Shortly</h5>
+      </div>
+    </div>
+  </section>
+
+  <section class="top_ranked py-5">
+    <div class="container py-md-3">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="headingbox text-center">
+            <p>How About Getting an Online Degree From IGNOU?</p>
+            <p>IGNOU is in itself a self governing authority directly under MHRD ( Ministry of Human Resource Development)</p>
+          </div>
+          <div class="owl-carousel owl-theme topRanked">
+    <div class="item position-relative py-2">
+        <div class="ranked-img">
+            <img src="/assets/images/UGC.png" alt="Approval">
+        </div>
+        <div class="ranked-body bg-white border">
+            <p class="mb-0">Approved by UGC ( University Grants commission )</p>
+        </div>
+    </div>
+    <div class="item position-relative py-2">
+        <div class="ranked-img">
+            <img src="/assets/images/AIU.png" alt="Approval">
+        </div>
+        <div class="ranked-body bg-white border">
+            <p class="mb-0">Approved by the Association of Indian Universities (AIU)</p>
+        </div>
+    </div>
+    <div class="item position-relative py-2">
+        <div class="ranked-img">
+            <img src="/assets/images/AICTE.png" alt="Approval">
+        </div>
+        <div class="ranked-body bg-white border">
+            <p class="mb-0">Approved by All India Council for Technical Education (AICTE)</p>
+        </div>
+    </div>
+    <div class="item position-relative py-2">
+        <div class="ranked-img">
+            <img src="/assets/images/NAAC.png" alt="Approval">
+        </div>
+        <div class="ranked-body bg-white border">
+            <p class="mb-0">NAAC with A++</p>
+        </div>
+    </div>
+</div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="certificate_sec seciton_padding">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-sm-5">
+          <img src="/assets/images/ignou-bba-certificate.jpg" width="100%" alt="Certificate">
+        </div>
+        <div class="col-sm-7 ps-3 ps-sm-5">
+          <div class="headingbox mt-3 mt-sm-0">
+            <h2>Online programs from IGNOU University</h2>
+            <p><small>IGNOU provides you the benefits of e-lectures, counseling from academic advisors, career assistance, and others.</small></p>
+
+          </div>
+          <ul class="certificat_list">
+            <li>Specialized instruction by a part-time academic advisor depending on the subject </li>
+            <li>Library</li>
+            <li>Teleconferencing</li>
+            <li>Detailed information on university guidelines, protocols, processes, and schedules is provided</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class="bg-dark text-white py-3 py-sm-3">
+    <div class="container pb-5 pb-sm-0">
+      <div class="row" id="lpu_according">
+        <div class="col-sm-12 text-center">
+          <p class="mb-1">
+          Disclaimer: We act solely as an information partner and do not conduct or facilitate admissions to IGNOU. For admissions, please visit the official IGNOU website or contact the university directly. IGNOU University holds full rights to request changes or removal of any non-relevant content. Images used are for illustrative purposes only and do not directly represent the respective colleges or universities. </p>
+                    <a href="https://degreeadmission.college/open-university/privacy-policy"> Privacy-Policy</a> & <a href="https://degreeadmission.college/open-university/terms-conditions">terms-conditions</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+
+  <script>
+    $('.topRanked').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 4
+        }
+      }
+    })
+  </script>
+
+</body>
+
+</html>
